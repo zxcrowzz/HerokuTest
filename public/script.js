07272ea3ac141a78f8cdfc13817a9c7c53262d08
zxcrowzz <pantsbro4@gmail.com>
2024-10-05 06:55:39 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/');
 const videogrid = document.getElementById('video-grid');
 const peers = {};
 const newPeer = new Peer(undefined, {
-    host: '/',
+    host: 'https://r3dxx-9ce6f110c87b.herokuapp.com',
     port: ''
 });
 const myVideo = document.createElement('video');
